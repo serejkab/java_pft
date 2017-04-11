868c208e2be888b68406dadd8e62d331102122c7
serejkab <babakhin.sergey@gmail.com>
2017-04-11 20:34:34 +0300

    Добавлена программа для вычисления расстояния между двумя точками

---
+++ sandbox/src/main/java/prog/lesson/one/sandbox/MyFirstProgram.java
@@ -0,0 +1,18 @@
+package prog.lesson.one.sandbox;
+
+public class MyFirstProgram {
+
+
+	public static void main(String[] args) {
+
+		Point p = new Point(1,2,3,4);
+
+
+
+		System.out.println("Расстояние между точками = " + p.distance());
+
+	}
+
+
+
+}
