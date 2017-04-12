358a844d643c38474f50a59437d44b7ca41c9893
serejkab <babakhin.sergey@gmail.com>
2017-04-12 21:27:09 +0300

    Исправлена программа для вычисления расстояния между двумя точками

--- sandbox/src/main/java/prog/lesson/one/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/prog/lesson/one/sandbox/MyFirstProgram.java
@@ -3,16 +3,25 @@ package prog.lesson.one.sandbox;
 public class MyFirstProgram {
 
 
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 
-		Point p = new Point(1,2,3,4);
+			Point p1 = new Point(1, 2);
+
+			Point p2 = new Point(3, 4);
 
 
+			System.out.println("Расстояние между точками = " + distance(p1, p2));
 
-		System.out.println("Расстояние между точками = " + p.distance());
+			System.out.println("Расстояние между точками = " + p2.distance(p1));
 
-	}
 
+		}
+
+		public static double distance(Point p1, Point p2){
+
+			return Math.sqrt(Math.pow((p1.у - p1.х),2) + Math.pow((p2.у - p2.х),2));
+
+		}
 
 
 }
--- sandbox/src/main/java/prog/lesson/one/sandbox/Point.java
+++ sandbox/src/main/java/prog/lesson/one/sandbox/Point.java
@@ -3,26 +3,32 @@ package prog.lesson.one.sandbox;
 /**
  * Created by SerejKa on 11.04.2017.
  */
-public class Point {
 
-     double x0;
-     double x1;
-     double y0;
-     double y1;
 
-     public Point (double x0, double x1, double y0, double y1 ){
+class Point {
+    double х, у;
 
-         this.x0 = x0;
-         this.x1 = x1;
-         this.y0 = y0;
-         this.y1 = y1;
+   public Point(double х, double у) {
 
-     }
-
-    public double distance() {
-
-        return  Math.sqrt(Math.pow((this.x1 - this.x0),2) + Math.pow((this.y1 - this.y0),2));
+       this.х = х;
+       this.у = у;
 
     }
 
-}
+  public   double distance(double х, double у) {
+
+       this.х = х;
+       this.у = у;
+
+        return Math.sqrt(Math.pow((х - у),2) + Math.pow((х - у),2));
+
+    }
+
+  public  double distance(Point p) {
+
+
+        return distance(p.х, p.у);
+
+    }
+ }
+
